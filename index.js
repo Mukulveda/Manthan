d61da2f7cc1c62cd45640b4a1b017d12857a4e70
Mukulveda <jainmukul0312@gmail.com>
2025-04-22 21:43:32 +0530

    Update index.js
    new changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
-
+ 
 // connect database
 connectDB();
 
